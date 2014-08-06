efb35812c5823fe5c646745afb99278c0c7b7141
Ubuntu <ubuntu@ip-172-31-47-16.us-west-2.compute.internal>
2014-08-06 15:07:51 +0000

    first commit

---
+++ includes/constants.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'local.ignore';
+
+define('BASEDIR',realpath(dirname(__FILE__).'/..'));
+
+define('DB_SERVER','localhost');
+define('DB_USER','email-line');
+define('DB_PASSWORD',HIDDEN_PASSWORD);
+define('DB_NAME','emailline');
