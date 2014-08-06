bca50126d16123daeb918e6e76d7628c34ab33eb
Ubuntu <ubuntu@ip-172-31-47-16.us-west-2.compute.internal>
2014-08-06 20:29:43 +0000

    add namespaces to autoloader

--- autoload.php
+++ autoload.php
@@ -4,5 +4,8 @@ require_once 'includes/constants.php';
 
 require_once 'Zend/Loader/StandardAutoloader.php';
 $loader=new Zend\Loader\StandardAutoloader(array('autoregister_zf'=>true));
-
+$loader->registerNamespaces(array(
+		'EmailLine\User'=>'src/EmailLine/User',
+		'EmailLine\Test'=>'src/EmailLine/Test',
+	));
 $loader->register();
