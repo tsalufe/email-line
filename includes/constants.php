2743e21a1b03a7c1ad44652e3e0e59423b1bed1a
Ubuntu <ubuntu@ip-172-31-47-16.us-west-2.compute.internal>
2014-08-06 20:28:47 +0000

    add global EMAILLINE_PDO

--- includes/constants.php
+++ includes/constants.php
@@ -7,3 +7,5 @@ define('DB_SERVER','localhost');
 define('DB_USER','email-line');
 define('DB_PASSWORD',HIDDEN_PASSWORD);
 define('DB_NAME','emailline');
+
+if(!isset($EMAILLINE_PDO)) $EMAILLINE_PDO=new PDO('mysql:host=localhost;dbname='.DB_NAME,DB_USER,DB_PASSWORD);
