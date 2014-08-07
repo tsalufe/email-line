8d477d8ba97d7363e60bfec61835f16f7dd17a11
Ubuntu <ubuntu@gehwah.com>
2014-08-07 01:55:58 +0000

    postfix.php: process email

--- postfix.php
+++ postfix.php
@@ -1,6 +1,27 @@
 #!/usr/bin/php
 <?php
-$file = fopen("/tmp/postfixtest", "a");
-fwrite($file, "Script successfully ran at ".date("Y-m-d H:i:s")."\n");
-fclose($file);
+require_once 'autoload.php';
+require_once 'includes/constants.php';
+require_once 'includes/globals.php';
+
+
+$stream=fopen('php://stdin','r');
+//$stream=fopen('test.f','r');
+fgets($stream);//drop the first line
+$raw_email='';
+while(!feof($stream)){
+	$raw_email.=fgets($stream);
+}
+fclose($stream);
+
+$zf_msg=Zend\Mail\Message::fromString($raw_email);
+
+$test=fopen('/tmp/postfixtest','a');
+ob_start();
+var_dump($zf_msg->getFrom());
+$from=ob_get_contents();
+ob_end_clean();
+fwrite($test,$from);
+fclose($test);
+
 ?>
