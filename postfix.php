1c0763edae625d1f32c25df3be6aef0a3e3beb93
Ubuntu <ubuntu@gehwah.com>
2014-08-08 02:19:25 +0000

    get from,to,subject,body from Zend\Mail\Message

--- postfix.php
+++ postfix.php
@@ -17,11 +17,22 @@ fclose($stream);
 $zf_msg=Zend\Mail\Message::fromString($raw_email);
 
 $test=fopen('/tmp/postfixtest','a');
-ob_start();
-var_dump($zf_msg->getFrom());
-$from=ob_get_contents();
-ob_end_clean();
-fwrite($test,$from);
+$from=$zf_msg->getFrom()->current()->getEmail();
+$to=$zf_msg->getTo()->current()->getEmail();
+$subject=$zf_msg->getSubject();
+$body=rmTags($zf_msg->getBody());
+$command='';
+preg_match('/Command=([a-z]*)/',$body,$c_match);
+if(count($c_match)>0){
+	$command=$c_match[1];
+}
+if($command!==''){
+}
+fwrite($test,"\n".$subject.' '.$from.' '.$to.' '.date('Y-m-d H:i:s'));
 fclose($test);
+
+function rmTags($str){
+	return preg_replace('/<[^<>]*>/','',$str);
+}
 
 ?>
